89125bee876158ad7afa5c419946cd16eb8f5454
Marshall Bowers <elliott.codes@gmail.com>
2015-10-16 18:33:13 -0400

    Restore window when activating

--- LoLMasteryManager/HwndInterface.vb
+++ LoLMasteryManager/HwndInterface.vb
@@ -241,6 +241,74 @@ Public Enum WM As UInteger
     XBUTTONUP = &H20C
 End Enum
 
+''' <summary>
+''' Possible nCmdShow values for ShowWindow.
+''' </summary>
+''' <remarks>https://msdn.microsoft.com/en-us/library/windows/desktop/ms633548(v=vs.85).aspx</remarks>
+Public Enum SW As UInteger
+
+    ''' <summary>
+    ''' Hides the window and activates another window.
+    ''' </summary>
+    HIDE = 0
+
+    ''' <summary>
+    ''' Activates and displays a window. If the window is minimized or maximized, the system restores it to its original size and position. An application should specify this flag when displaying the window for the first time.
+    ''' </summary>
+    SHOWNORMAL = 1
+
+    ''' <summary>
+    ''' Activates the window and displays it as a minimized window.
+    ''' </summary>
+    SHOWMINIMIZED = 2
+
+    ''' <summary>
+    ''' Maximizes the specified window.
+    ''' </summary>
+    MAXIMIZE = 3
+
+    ''' <summary>
+    ''' Activates the window and displays it as a maximized window.
+    ''' </summary>
+    SHOWMAXIMIZED = 3
+
+    ''' <summary>
+    ''' Displays a window in its most recent size and position. This value is similar to SW_SHOWNORMAL, except that the window is not activated.
+    ''' </summary>
+    SHOWNOACTIVATE = 4
+
+    ''' <summary>
+    ''' Activates the window and displays it in its current size and position.
+    ''' </summary>
+    SHOW = 5
+
+    ''' <summary>
+    ''' Displays the window as a minimized window. This value is similar to <see cref="SHOWMINIMIZED"/>, except the window is not activated.
+    ''' </summary>
+    SHOWMINNOACTIVE = 7
+
+    ''' <summary>
+    ''' Displays the window in its current size and position. This value is similar to <see cref="SHOW"/>, except that the window is not activated.
+    ''' </summary>
+    SHOWNA = 8
+
+    ''' <summary>
+    ''' Activates and displays the window. If the window is minimized or maximized, the system restores it to its original size and position. An application should specify this flag when restoring a minimized window.
+    ''' </summary>
+    RESTORE = 9
+    ''' <summary>
+    ''' Sets the show state based on the SW_ value specified in the STARTUPINFO structure passed to the CreateProcess function by the program that started the application.
+    ''' </summary>
+    ''' 
+    SHOWDEFAULT = 10
+
+    ''' <summary>
+    ''' Minimizes a window, even if the thread that owns the window is not responding. This flag should only be used when minimizing windows from a different thread.
+    ''' </summary>
+    FORCEMINIMIZE = 11
+
+End Enum
+
 <StructLayout(LayoutKind.Sequential)>
 Public Structure RECT
 
@@ -258,7 +326,7 @@ Friend NotInheritable Class HwndInterface
 
     Public Shared Function ActivateWindow(hwnd As IntPtr) As Boolean
 
-        Return SetForegroundWindow(hwnd)
+        Return ShowWindow(hwnd, SW.RESTORE) AndAlso SetForegroundWindow(hwnd)
 
     End Function
 
@@ -267,6 +335,10 @@ Friend NotInheritable Class HwndInterface
         SendMessage(hwnd, &HF5, IntPtr.Zero, IntPtr.Zero)
 
     End Sub
+
+    <DllImport("user32.dll")>
+    Private Shared Function ShowWindow(hWnd As IntPtr, nCmdShow As UInteger) As Boolean
+    End Function
 
     <DllImport("user32.dll")>
     Private Shared Function CloseWindow(hWnd As IntPtr) As Boolean
